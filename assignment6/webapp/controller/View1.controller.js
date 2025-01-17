1889ca6bfc7fca7376a96a3fd4a3ee103d6e533c
Kanyadhara Akash <akash.kanyadhara@yash.com>
2025-01-17 11:48:48 +0000

    assignment8_updated2

--- assignment6/webapp/controller/View1.controller.js
+++ assignment6/webapp/controller/View1.controller.js
@@ -1,43 +1,234 @@
 sap.ui.define([
-    'sap/ui/core/mvc/Controller',
-    'sap/ui/model/json/JSONModel'
-], function(Controller, JSONModel) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+    "sap/ui/model/Sorter",
+    "sap/m/MessageBox",
+], function (Controller, JSONModel, Filter, FilterOperator, Sorter, MessageBox) {
     "use strict";
-
-    var TableController = Controller.extend("assignment6.controller.View1", {
-
-        formatter: {
-            // Example formatter function for price
-            formatPrice: function(price) {
-                return parseFloat(price).toFixed(2);
-            },
-
-            // Formatter function for availability state
-            formatAvailabilityState: function(availability) {
-                switch (availability) {
-                    case "Available":
-                        return "Success";
-                    case "Out of Stock":
-                        return "Error";
-                    case "Discontinued":
-                        return "Warning";
-                    default:
-                        return "None";
-                }
+   
+    return Controller.extend("assignment6.controller.View1", {
+       
+        onInit: function () {
+            this.oRouter = this.getOwnerComponent().getRouter();
+            var dataModel = new JSONModel("/model/data.json");
+            this.getView().setModel(dataModel, "products"); // Ensure the model name matches
+            this.getView().setModel(dataModel, "value");
+        
+            var valueModel = this.getView().getModel("value");
+            valueModel.setProperty("/shoppingCartItems", []);
+            valueModel.setProperty("/savingForLaterItems", []);
+        },
+ 
+        formatAvailabilityState: function (sAvailability) {
+            var states = {
+                "Available": "Success",
+                "Discontinued": "Error",
+                "Out of Stock": "Warning"
+            };
+            return states[sAvailability] || "None";
+        },
+ 
+        onSearch: function (oEvent) {
+            var sQuery = oEvent.getParameter("query");
+            var oTableSearchState = sQuery ? [new Filter("name", FilterOperator.Contains, sQuery)] : [];
+            this.getView().byId("productsTable").getBinding("items").filter(oTableSearchState, "Application");
+        },
+ 
+        onSort: function () {
+            this._bDescendingSort = !this._bDescendingSort;
+            var oBinding = this.getView().byId("productsTable").getBinding("items");
+            oBinding.sort(new Sorter("name", this._bDescendingSort));
+        },
+ 
+        onListItemPress: function (event) {
+            var oFlexibleColumnLayout = this.byId("flexibleColumnLayout");
+            oFlexibleColumnLayout.setLayout("TwoColumnsMidExpanded");
+ 
+            var selectedItemName = event.getParameter("listItem").getBindingContext("value").getProperty("name");
+            var dataModel = this.getView().getModel("products");
+            var selectedProduct = dataModel.getProperty("/Products").find(function (product) {
+                return product.name === selectedItemName;
+            });
+ 
+            if (selectedProduct) {
+                var oView = this.getView();
+                oView.byId("productImage").setSrc(selectedProduct.photo);
+                oView.byId("nameAttribute").setTitle(" Prodct name:" + selectedProduct.name);
+                oView.byId("priceAttribute").setText(" " + selectedProduct.price);
+                oView.byId("currencyAttribute").setText("Currency " + selectedProduct.currency);
+                oView.byId("availabilityAttribute").setText(" " + selectedProduct.availability);
+                oView.byId("supplierAttribute").setText("Supplier: " + selectedProduct.supplier);
+                oView.byId("descriptionAttribute").setText("Description: " + selectedProduct.description);
+                oView.byId("weightAttribute").setText("Weight: " + selectedProduct.weight);
+                oView.byId("measurementsAttribute").setText("Measurements: " + selectedProduct.measurements);
             }
         },
+ 
+        onImagePress: function (event) {
+            var oDialog = new sap.m.Dialog({
+                content: new sap.m.Image({ src: event.getSource().getSrc(), width: "80%" }),
+                beginButton: new sap.m.Button({ text: "Close", press: function () { oDialog.close(); } }),
+                afterClose: function () { oDialog.destroy(); }
+            });
+            oDialog.open();
+        },
+ 
+        onShowCardPress: function () {
+            var valueModel = this.getView().getModel("value");
+            valueModel.setProperty("/shoppingCartItems", []);
+            valueModel.setProperty("/savingForLaterItems", []);
+            this.getView().byId("editToolbar").setVisible(true);
+            this.byId("flexibleColumnLayout").setLayout("ThreeColumnsMidExpanded");
+        },
+ 
+        onEditPress: function () {
+            this.byId("shoppingCartList").setMode("Delete");
+            this.getView().byId("editToolbar").setVisible(false);
+        },
+ 
+        onDeletePress: function (oEvent) {
+            var oListItem = oEvent.getParameter("listItem");
+            var oList = this.byId("shoppingCartList");
+            var aItems = this.getView().getModel("value").getProperty("/shoppingCartItems");
+            var iIndex = oList.indexOfItem(oListItem);
+ 
+            if (iIndex !== -1) {
+                aItems.splice(iIndex, 1);
+                this.getView().getModel("value").setProperty("/shoppingCartItems", aItems);
+                oList.removeItem(oListItem);
+                this.updateTotalPrice();
+            }
+        },
+ 
+        onAddToCartPress: function () {
+            var oView = this.getView();
+            var availability = oView.byId("availabilityAttribute").getText().trim();
+ 
+            if (availability === "Discontinued") {
+                MessageBox.error("This product has been discontinued and cannot be ordered anymore.");
+                return;
+            }
+ 
+            var product = {
+                name: oView.byId("nameAttribute").getTitle().trim(),
+                price: parseFloat(oView.byId("priceAttribute").getText().trim()),
+                currency: oView.byId("currencyAttribute").getText().trim(),
+                availability: availability
+            };
+ 
+            var handleAddToCart = function () {
+                var shoppingCartList = this.byId("shoppingCartList");
+                var savingForLaterList = this.byId("savingForLaterList");
+                var savingForLaterItems = savingForLaterList.getItems();
+ 
+                var itemToRemove = savingForLaterItems.find(function (item) {
+                    return item.getContent()[0].getItems()[0].getText() === product.name;
+                });
+ 
+                if (itemToRemove) {
+                    savingForLaterList.removeItem(itemToRemove);
+                }
+ 
+                var newListItem = new sap.m.CustomListItem({
+                    content: [
+                        new sap.m.VBox({
+                            items: [
+                                new sap.m.Title({ text: product.name, level: "H3" }),
+                                new sap.m.VBox({
+                                    items: [
+                                        new sap.m.Text({ text: "Price: " + product.price }),
+                                        new sap.m.Text({ text: "Currency: " + product.currency }),
+                                        new sap.m.Text({ text: "Availability: " + product.availability })
+                                    ]
+                                })
+                            ]
+                        }),
+                        new sap.m.Link({ text: "Save for Later", press: this.onSaveForLaterPress.bind(this, product) })
+                    ]
+                });
+ 
+                shoppingCartList.addItem(newListItem);
+                this.updateTotalPrice(product.price);
+                this.byId("flexibleColumnLayout").setLayout("ThreeColumnsMidExpanded");
+            }.bind(this);
+ 
+            if (availability === "Out of Stock") {
+                MessageBox.confirm(
+                    "This product is out of stock. Do you still want to add it to the cart?", {
+                        actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+                        onClose: function (oAction) {
+                            if (oAction === MessageBox.Action.YES) {
+                                handleAddToCart();
+                            }
+                        }
+                    }
+                );
+            } else {
+                handleAddToCart();
+            }
+        },
+ 
+        updateTotalPrice: function () {
+            var totalPrice = this.byId("shoppingCartList").getItems().reduce(function (sum, item) {
+                var price = parseFloat(item.getContent()[0].getItems()[1].getItems()[0].getText().match(/\d+(?:\.\d+)?/));
+                return sum + price;
+            }, 0);
+ 
+            this.byId("totalPriceText").setText("Total Price: " + totalPrice.toFixed(2) + " EUR");
+        },
+ 
+        onSaveForLaterPress: function (product) {
+            var shoppingCartList = this.byId("shoppingCartList");
+            var shoppingCartItems = shoppingCartList.getItems();
+ 
+            var itemToRemove = shoppingCartItems.find(function (item) {
+                return item.getContent()[0].getItems()[0].getText() === product.name;
+            });
+ 
+            if (itemToRemove) {
+                shoppingCartList.removeItem(itemToRemove);
+            }
+ 
+            this.updateTotalPrice(0);
+ 
+            var newListItem = new sap.m.CustomListItem({
+                content: [
+                    new sap.m.VBox({
+                        items: [
+                            new sap.m.Title({ text: product.name, level: "H3" }),
+                            new sap.m.VBox({
+                                items: [
+                                    new sap.m.Text({ text: "Price: " + product.price + " " + product.currency }),
+                                    new sap.m.Text({ text: "Availability: " + product.availability })
+                                ]
+                            })
+                        ]
+                    }),
+                    new sap.m.Link({ text: "Add to shopping cart", press: this.onAddToCartPress.bind(this, product) })
+                ]
+            });
+ 
+            this.byId("savingForLaterList").addItem(newListItem);
+        },
+ 
+        onProceedPress: function () {
+            var shoppingCartItems = this.byId("shoppingCartList").getItems().map(function (item) {
+                var itemContent = item.getContent()[0].getItems();
+                return {
+                    name: itemContent[0].getText(),
+                    price: parseFloat(itemContent[1].getItems()[0].getText().split(" ")[1]),
+                    currency: itemContent[1].getItems()[0].getText().split(" ")[2],
+                    availability: itemContent[1].getItems()[1].getText().split(": ")[1]
+                };
+            });
 
-        onInit: function () {
-            // Create a JSON model
-            var oModel = new JSONModel();
-
-            // Load data from the JSON file
-            oModel.loadData("/model/data.json");
-
-            // Set the model to the view
-            this.getView().setModel(oModel);
+            if (shoppingCartItems.length > 0) {
+                this.oRouter.navTo("RouteView2", { selectedItems: JSON.stringify(shoppingCartItems) });
+            } else {
+                MessageBox.error("No items in the shopping cart.");
+            }
         }
     });
-
-    return TableController;
 });
--- assignment6/webapp/controller/View2.controller.js
+++ assignment6/webapp/controller/View2.controller.js
@@ -1,11 +1,83 @@
-sap.ui.define([
-    'sap/ui/core/mvc/Controller'
-], function(Controller) {
-    "use strict";
-
-    return Controller.extend("assignment6.controller.View2", {
-        onInit: function () {
-            // Initialization code if needed
-        }
-    });
-});
+<mvc:View controllerName="assignment6.controller.View2"
+    xmlns:mvc="sap.ui.core.mvc"
+    xmlns="sap.m"
+    xmlns:form="sap.ui.layout.form"
+    xmlns:core="sap.ui.core"
+    xmlns:u="sap.ui.unified">
+    
+    <Page>
+        <Wizard id="wizard">
+            <WizardStep title="Items">
+                <List id="itemList" items="{view>/items}">
+                    <StandardListItem title="{view>name}" description="{view>price} {view>currency}" />
+                </List>
+            </WizardStep>
+            <WizardStep title="Payment Method">
+                <VBox>
+                    <Text text="Select Payment Method:" />
+                    <RadioButton groupName="paymentGroup" select="onPaymentMethodSelect" text="Credit Card" data-key="CreditCard"/>
+                    <RadioButton groupName="paymentGroup" select="onPaymentMethodSelect" text="Cash on Delivery" data-key="CashOnDelivery"/>
+                    <RadioButton groupName="paymentGroup" select="onPaymentMethodSelect" text="Bank Transfer" data-key="BankTransfer"/>
+                </VBox>
+            </WizardStep>
+            <WizardStep title="Payment Details" id="paymentDetailsStep">
+                <VBox id="paymentDetailsVBox">
+                    <Text id="paymentDetailsText" text="Please enter your payment details." />
+                    <VBox id="creditCardDetails" visible="false">
+                        <Label text="Card Holder Name" required="true" />
+                        <Input id="cardHolderName" placeholder="Card Holder Name" required="true" />
+                        <Label text="Credit Card Number" required="true" />
+                        <Input id="creditCardNumber" placeholder="Credit Card Number" required="true" />
+                        <Label text="Expiry Date" required="true" />
+                        <Input id="expiryDate" placeholder="Expiry Date" required="true" />
+                        <Label text="Security Code" required="true" />
+                        <Input id="securityCode" placeholder="Security Code" required="true" />
+                    </VBox>
+                    <VBox id="cashOnDeliveryDetails" visible="false">
+                        <Label text="Billing Address" required="true" />
+                        <Input id="billingAddress" placeholder="Enter your billing address" required="true" />
+                    </VBox>
+                    <VBox id="bankTransferDetails" visible="false">
+                        <Label text="Bank Account Number" required="true" />
+                        <Input id="bankAccountNumber" placeholder="Enter your bank account number" required="true" />
+                        <Label text="Bank Name" required="true" />
+                        <Input id="bankName" placeholder="Enter your bank name" required="true" />
+                    </VBox>
+                </VBox>
+            </WizardStep>
+            <WizardStep title="Invoice Address">
+                <VBox>
+                    <Label text="Street" />
+                    <Input id="street" placeholder="Street" />
+                    <Label text="City" />
+                    <Input id="city" placeholder="City" />
+                    <Label text="Postal Code" />
+                    <Input id="postalCode" placeholder="Postal Code" />
+                </VBox>
+            </WizardStep>
+            <WizardStep title="Delivery Type">
+                <Select id="deliveryType">
+                    <core:Item key="standard" text="Standard" />
+                    <core:Item key="express" text="Express" />
+                </Select>
+            </WizardStep>
+            <WizardStep title="Review Order">
+                <VBox>
+                    <Text text="Order Summary" />
+                    <List id="orderSummaryList" items="{view>/items}">
+                        <StandardListItem title="{view>name}" description="{view>price} {view>currency}" />
+                    </List>
+                    <Text text="Payment Method: {view>/selectedPaymentMethod}" />
+                    <Text text="Card Holder Name: {view>/cardHolderName}" />
+                    <Text text="Credit Card Number: {view>/creditCardNumber}" />
+                    <Text text="Expiry Date: {view>/expiryDate}" />
+                    <Text text="Security Code: {view>/securityCode}" />
+                    <Text text="Billing Address: {view>/billingAddress}" />
+                    <Text text="Bank Account Number: {view>/bankAccountNumber}" />
+                    <Text text="Bank Name: {view>/bankName}" />
+                    <Text text="Invoice Address: {view>/invoiceAddress/street}, {view>/invoiceAddress/city}, {view>/invoice Address/postalCode}" />
+                </VBox>
+            </WizardStep>
+        </Wizard>
+    </Page>
+</mvc:View>
